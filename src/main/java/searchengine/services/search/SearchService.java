ed5971cee8580d16110963eee39960282a0b9a1a
Sergey Lunev <lunev.s@gmail.com>
2024-04-22 21:54:46 +0300

    refactor search service

--- src/main/java/searchengine/services/search/SearchService.java
+++ src/main/java/searchengine/services/search/SearchService.java
@@ -11,6 +11,7 @@ import searchengine.data.dto.LemmaFrequencyDto;
 import searchengine.data.dto.PageDto;
 import searchengine.data.dto.SearchResponse;
 import searchengine.services.scanner.PageService;
+import searchengine.tools.StringUtils;
 
 import java.text.MessageFormat;
 import java.util.ArrayList;
@@ -38,11 +39,7 @@ public class SearchService {
     private final PageService pageService;
 
     public SearchResponse searchStart(String query, String siteUrl, Integer offset, Integer limit) {
-        List<DetailedSearchItem> detailedSearchItemList = new ArrayList<>();
         Set<String> queryWords = lemmaParserService.collectLemmas(query).keySet();
-        log.info("initial search string {}", query);
-        log.info("search lemmas: {}", queryWords);
-
         List<LemmaFrequencyDto> searchLemmas = lemmaService.getLemmasFrequency(queryWords);
         List<LemmaFrequencyDto> filteredLemmas = searchLemmas.stream()
                 .filter(el -> el.getLemmaFrequency() < EXCLUDE_LIMIT)
@@ -50,45 +47,64 @@ public class SearchService {
                 .toList();
         Set<Integer> filteredLemmaIds = filteredLemmas.stream().map(LemmaFrequencyDto::getLemmaId).collect(Collectors.toSet());
 
-        if (!filteredLemmaIds.isEmpty()) {
-            List<PageDto> resultPages = pageService.getPagesWithAllLemmas(filteredLemmaIds);
-            resultPages.forEach(page -> log.info(page.getPagePath()));
-            log.info("search pages ids: {}", resultPages.stream().map(PageDto::getId).collect(Collectors.toSet()));
-            Set<Integer> allLemmasIds = searchLemmas.stream().map(LemmaFrequencyDto::getLemmaId).collect(Collectors.toSet());
-            log.info("search lemmas ids: {}", allLemmasIds);
-            List<FinalSearchResultDto> result = lemmaService.getFinalSearchResultDto(
-                    allLemmasIds,
-                    resultPages.stream().map(PageDto::getId).collect(Collectors.toSet()));
-            log.info("result pages: {}", result.size());
-
-            Map<String, Set<Integer>> siteFrequencies = result.stream()
-                    .collect(Collectors.groupingBy(
-                            FinalSearchResultDto::getSiteUrl,
-                            Collectors.mapping(FinalSearchResultDto::getAbsFrequency, Collectors.toSet())));
-            for (Map.Entry<String, Set<Integer>> entry : siteFrequencies.entrySet()) {
-                Optional<Integer> max = siteFrequencies.get(entry.getKey()).stream().max(Float::compare);
-                max.ifPresent(value -> result.forEach(el -> el.setRelFrequency((double) el.getAbsFrequency() / value)));
-            }
-            result.stream()
-                    .sorted(Comparator.comparingInt(FinalSearchResultDto::getAbsFrequency).reversed())
-                    .limit(20)
-                    .forEach(el -> {
-                        Document doc = Jsoup.parse(el.getPageContent());
-                        String text = doc.body().text().toLowerCase();
-                        log.info("{}\t{}\t{}\t{}\t{}", el.getSiteUrl(), el.getPagePath(), el.getAbsFrequency(), el.getRelFrequency(), doc.title());
-                        String snippet = getIndexes(text, queryWords.stream().toList());
-                        log.info("snippet: {}", snippet);
-                        DetailedSearchItem item = new DetailedSearchItem(el.getSiteUrl(), el.getSiteName(), el.getPagePath(), doc.title(), snippet, el.getRelFrequency());
-                        detailedSearchItemList.add(item);
-                    });
+        if (filteredLemmaIds.isEmpty()) {
+            return new SearchResponse(false, 0, null);
         }
-
+        List<FinalSearchResultDto> finalSearchResults = getFinalSearchResultDto(filteredLemmaIds, searchLemmas);
+        List<DetailedSearchItem> detailedSearchItemList = finalSearchResults.stream()
+                .sorted(Comparator.comparingInt(FinalSearchResultDto::getAbsFrequency).reversed())
+                .limit(20)
+                .map(el -> formatDetailedSearchItem(el,queryWords))
+                .toList();
         return new SearchResponse(
                 !detailedSearchItemList.isEmpty(),
                 detailedSearchItemList.size(),
-                detailedSearchItemList
-        );
+                detailedSearchItemList);
+
+//                    .forEach(el -> {
+//                        Document doc = Jsoup.parse(el.getPageContent());
+//                        String text = doc.body().text().toLowerCase();
+//                        String snippet = getSnippet(text, queryWords.stream().toList());
+//                        DetailedSearchItem item = new DetailedSearchItem(el.getSiteUrl(), el.getSiteName(), el.getPagePath(), doc.title(), snippet, el.getRelFrequency());
+//                        detailedSearchItemList.add(item);
+//                    });
+
     }
+
+    private DetailedSearchItem formatDetailedSearchItem(FinalSearchResultDto searchResultDto, Set<String> queryWords) {
+        Document doc = Jsoup.parse(searchResultDto.getPageContent());
+        String text = doc.body().text().toLowerCase();
+        String snippet = getSnippet(text, queryWords.stream().toList());
+        return new DetailedSearchItem(searchResultDto.getSiteUrl(), searchResultDto.getSiteName(), searchResultDto.getPagePath(), doc.title(), snippet, searchResultDto.getRelFrequency());
+    }
+
+    private List<FinalSearchResultDto> getFinalSearchResultDto(Set<Integer> filteredLemmaIds, List<LemmaFrequencyDto> searchLemmas) {
+        List<PageDto> resultPages = pageService.getPagesWithAllLemmas(filteredLemmaIds);
+        Set<Integer> allLemmasIds = searchLemmas.stream().map(LemmaFrequencyDto::getLemmaId).collect(Collectors.toSet());
+        List<FinalSearchResultDto> result = lemmaService.getFinalSearchResultDto(
+                allLemmasIds,
+                resultPages.stream().map(PageDto::getId).collect(Collectors.toSet()));
+
+        Map<String, Set<Integer>> siteFrequencies = result.stream()
+                .collect(Collectors.groupingBy(
+                        FinalSearchResultDto::getSiteUrl,
+                        Collectors.mapping(FinalSearchResultDto::getAbsFrequency, Collectors.toSet())));
+        for (Map.Entry<String, Set<Integer>> entry : siteFrequencies.entrySet()) {
+            Optional<Integer> max = siteFrequencies.get(entry.getKey()).stream().max(Float::compare);
+            max.ifPresent(value -> result.forEach(el -> el.setRelFrequency((double) el.getAbsFrequency() / value)));
+        }
+        return result;
+    }
+
+    public String getSnippet(String text, List<String> words) {
+        Map<String, Set<String>> searchFormWords = getNormalFormsMap(text, words);
+        List<NavigableSet<Integer>> indexes = findWordsIndexes(text, searchFormWords);
+        List<Integer> indexCombination = findClosestWordCombination(indexes);
+        String snippet = StringUtils.createSnippet(text, indexCombination);
+
+        return makeSearchWordsBold(indexCombination, text, snippet);
+    }
+
 
     private Integer getClosestElement(Integer baseEl, Integer el1, Integer el2) {
         if (el1 == null && el2 == null) {
@@ -100,8 +116,7 @@ public class SearchService {
         }
     }
 
-    public String getIndexes(String text, List<String> words) {
-        String snippet = "";
+    private Map<String, Set<String>> getNormalFormsMap(String text, List<String> words) {
         Map<String, Set<String>> searchFormWords = new HashMap<>();
         words.forEach(w -> searchFormWords.put(w, new HashSet<>()));
 
@@ -112,8 +127,40 @@ public class SearchService {
                 searchFormWords.get(words.get(wordIndex)).add(curWord);
             }
         }
+        return searchFormWords;
+    }
 
-        List<NavigableSet<Integer>> indexes = new ArrayList<>(words.size());
+    private List<Integer> findClosestWordCombination(List<NavigableSet<Integer>> indexes)  {
+        List<Integer> indexCombination = new ArrayList<>();
+        NavigableSet<Integer> baseSet = indexes.remove(0);
+        if (baseSet == null || baseSet.isEmpty()) {
+            return new ArrayList<>();
+        }
+        for (Integer curIndex : baseSet) {
+            List<Integer> curCombination = new ArrayList<>(List.of(curIndex));
+            for (NavigableSet<Integer> curSet : indexes) {
+                Integer closest = getClosestElement(curIndex, curSet.ceiling(curIndex), curSet.floor(curIndex));
+                if (closest != null) {
+                    curCombination.add(closest);
+                }
+            }
+            if (indexCombination.isEmpty()) {
+                indexCombination.addAll(curCombination);
+            } else {
+                if (getCombinationLength(curCombination) < getCombinationLength(indexCombination)) {
+                    indexCombination = new ArrayList<>(curCombination);
+                }
+            }
+        }
+        return indexCombination.stream().sorted().toList();
+    }
+
+    private int getCombinationLength(List<Integer> combination) {
+        return combination.stream().max(Integer::compareTo).orElse(Integer.MAX_VALUE) - combination.stream().min(Integer::compareTo).orElse(0);
+    }
+
+    private List<NavigableSet<Integer>> findWordsIndexes(String text, Map<String, Set<String>> searchFormWords) {
+        List<NavigableSet<Integer>> indexes = new ArrayList<>(searchFormWords.size());
         for (Map.Entry<String, Set<String>> searchElement : searchFormWords.entrySet()) {
             NavigableSet<Integer> curIndexes = new TreeSet<>();
             for (String s : searchElement.getValue()) {
@@ -124,79 +171,18 @@ public class SearchService {
                 }
             }
             indexes.add(curIndexes);
-            log.info("{} indexes = {}", searchElement.getKey(), curIndexes);
         }
-        List<Integer> indexCombination = new ArrayList<>();
-        NavigableSet<Integer> baseSet = indexes.remove(0);
-        if (baseSet != null) {
-            for (Integer curIndex : baseSet) {
-                List<Integer> curCombination = new ArrayList<>();
-                curCombination.add(curIndex);
-                for (NavigableSet<Integer> curSet : indexes) {
-                    Integer closest = getClosestElement(curIndex, curSet.ceiling(curIndex), curSet.floor(curIndex));
-                    if (closest == null) {
-                        continue;
-                    }
-                    curCombination.add(closest);
-                }
-                if (indexCombination.isEmpty()) {
-                    indexCombination.addAll(curCombination);
-                } else {
-                    int oldCombinationLen = indexCombination.stream().max(Integer::compareTo).orElse(Integer.MAX_VALUE) - indexCombination.stream().min(Integer::compareTo).orElse(0);
-                    int newCombinationLen = curCombination.stream().max(Integer::compareTo).orElse(Integer.MAX_VALUE) - curCombination.stream().min(Integer::compareTo).orElse(0);
-                    if (newCombinationLen < oldCombinationLen) {
-                        indexCombination = new ArrayList<>(curCombination);
-                    }
-                }
-            }
-            indexCombination = indexCombination.stream().sorted().toList();
-            List<String> snippetPieces = getStrings(text, indexCombination);
-            snippet = String.join(" ... ", snippetPieces);
+        return indexes;
+    }
 
-            for (int wordStartIndex : indexCombination) {
-                int wordEndIndex = text.indexOf(" ", wordStartIndex+1);
-                String curWordToBold = text.substring(wordStartIndex, wordEndIndex);
-                log.info("make bold word: {}", curWordToBold);
-                snippet = snippet.replace(curWordToBold, MessageFormat.format("<b>{0}</b>", curWordToBold));
-            }
+    private String makeSearchWordsBold(List<Integer> indexCombination, String text, String snippet) {
+        for (int wordStartIndex : indexCombination) {
+            int wordEndIndex = text.indexOf(" ", wordStartIndex+1);
+            String curWordToBold = text.substring(wordStartIndex, wordEndIndex);
+            snippet = snippet.replace(curWordToBold, MessageFormat.format("<b>{0}</b>", curWordToBold));
         }
-
         return snippet;
     }
 
-    private List<String> getStrings(String text, List<Integer> indexCombination) {
-        List<String> snippetPieces = new ArrayList<>(indexCombination.size());
-        int startIndex = 0;
-        int endIndex = 0;
-        for (int curIndex : indexCombination) {
-            if (endIndex == 0) {
-                startIndex = shiftLeft(text, curIndex);
-                endIndex = shiftRight(text, curIndex);
-                continue;
-            }
-            if (curIndex - endIndex > 100) {
-                String curPiece = text.substring(startIndex, endIndex);
-                snippetPieces.add(curPiece);
-
-                startIndex = shiftLeft(text, curIndex);
-                endIndex = shiftRight(text, curIndex);
-            } else {
-                endIndex = shiftRight(text, curIndex);
-            }
-        }
-        String curPiece = text.substring(startIndex, endIndex);
-        snippetPieces.add(curPiece);
-        return snippetPieces;
-    }
-
-    private int shiftLeft(String text, int curIndex) {
-        int tmpIndex = text.indexOf(" ", curIndex - 50);
-        return (tmpIndex == -1 || tmpIndex > curIndex) ? curIndex : tmpIndex;
-    }
-
-    private int shiftRight(String text, int curIndex) {
-        int tmpIndex = text.indexOf(" ", curIndex + 30);
-        return (tmpIndex == -1) ? text.length() : tmpIndex;
-    }
 
 }
--- src/main/java/searchengine/tools/StringUtils.java
+++ src/main/java/searchengine/tools/StringUtils.java
@@ -1,5 +1,38 @@
 package searchengine.tools;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringUtils {
+
+    public static String createSnippet(String text, List<Integer> indexCombination) {
+        List<String> snippetPieces = new ArrayList<>(indexCombination.size());
+        int startIndex = 0;
+        int endIndex = 0;
+        for (int curIndex : indexCombination) {
+            if (endIndex == 0) {
+                startIndex = shiftLeft(text, curIndex);
+                endIndex = shiftRight(text, curIndex);
+                continue;
+            }
+            if (curIndex - endIndex > 100) {
+                snippetPieces.add(text.substring(startIndex, endIndex));
+                startIndex = shiftLeft(text, curIndex);
+            }
+            endIndex = shiftRight(text, curIndex);
+        }
+        snippetPieces.add(text.substring(startIndex, endIndex));
+        return String.join(" ... ", snippetPieces);
+    }
+
+    private static int shiftLeft(String text, int curIndex) {
+        int tmpIndex = text.indexOf(" ", curIndex - 50);
+        return (tmpIndex == -1 || tmpIndex > curIndex) ? curIndex : tmpIndex;
+    }
+
+    private static int shiftRight(String text, int curIndex) {
+        int tmpIndex = text.indexOf(" ", curIndex + 30);
+        return (tmpIndex == -1) ? text.length() : tmpIndex;
+    }
 
 }
